80198bd621bd83a7eaf6a54ca02c9fc36059d90f
frostie <mattwiner04@gmail.com>
2025-04-23 16:37:27 -0700

    1

---
+++ discList.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef DISCLIST_H
+#define DISCLIST_H
+#include <iostream>
+struct disc {
+	int data;
+	Node* next = nullptr;
+};i 
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void 
+int main(int argc, char *argv[]) {
+	cout << "HELLO WORLD\n";
+	cout << "READING DVD..." << endl;
+	string dvdPath = "/dev/sr0";
+	ifstream dvdFile(dvdPath,ios::binary);
+	if (!dvdFile.is_open()) {
+		cerr << "ERROR OPENING DVD drive. " << endl;
+	}
+	// Read DVD Structure
+	// Read VOB data
+	// Encode Video data using FFMPEG
+	// Write Encoded Data To File
+	
+	dvdFile.close();
+	return 0;
+}
